14dfc1059edcd8c782b5ce84fcb2f8b79fa14118
Adrian Kaluzinski <akaluzin@akamai.com>
2023-01-12 17:18:34 +0100

    Initialize spring boot app

---
+++ src/main/java/eu/kaluzinski/wordpressjpa/WordpressJpaApplication.java
@@ -0,0 +1,13 @@
+package eu.kaluzinski.wordpressjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordpressJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordpressJpaApplication.class, args);
+    }
+
+}
