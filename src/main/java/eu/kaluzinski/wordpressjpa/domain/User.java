20ca695943c16d8e076a35ef44e563c85a8b70ff
Adrian Kaluzinski <akaluzin@akamai.com>
2023-01-14 22:13:00 +0100

    Add validation to user closes #1

--- src/main/java/eu/kaluzinski/wordpressjpa/domain/User.java
+++ src/main/java/eu/kaluzinski/wordpressjpa/domain/User.java
@@ -6,8 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 import java.sql.Timestamp;
 
@@ -21,31 +25,49 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Column(name = "user_login")
+    @Size(max = 60)
     private String login;
 
+    @NotNull
     @Column(name = "user_pass")
+    @Size(max = 255)
     private String password;
 
+    @NotNull
     @Column(name = "user_nicename")
+    @Size(max = 50)
     private String nicename;
 
+    @NotNull
     @Column(name = "user_email")
+    @Size(max = 100)
+    @Email
     private String email;
 
+    @URL
+    @NotNull
     @Column(name = "user_url")
+    @Size(max = 100)
     private String url;
 
+    @NotNull
     @Column(name = "user_registered")
     private Timestamp registered;
 
+    @NotNull
     @Column(name = "user_activation_key")
+    @Size(max = 255)
     private String activationKey;
 
-    @Column(name = "user_status", nullable = false)
+    @NotNull
+    @Column(name = "user_status")
     private Integer status;
 
+    @NotNull
     @Column(name = "display_name")
+    @Size(max = 255)
     private String displayName;
 
 }
--- src/main/java/eu/kaluzinski/wordpressjpa/domain/UserMeta.java
+++ src/main/java/eu/kaluzinski/wordpressjpa/domain/UserMeta.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,7 +23,9 @@ public class UserMeta {
     @Column(name = "umeta_id")
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "user_id")
     private Long userId;
 
     @Column(name = "meta_key")
